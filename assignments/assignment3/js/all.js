422e7c9039d237c4f578dd73c0cf1ebbd587621e
HasnainShafeeq <hasnainshafeeq1997@gmail.com>
2020-05-08 06:11:30 +0500

    First Commit

---
+++ assignments/assignment3/js/all.js
@@ -0,0 +1,12 @@
+//animated
+new WOW().init();
+
+//ToTop
+$(document).ready(function(){
+    $("#totop").Totop(
+        options = {
+        scrolltime:800,
+        image:"images/top2.png"
+        }
+    );
+});
